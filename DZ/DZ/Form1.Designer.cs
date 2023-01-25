46972f9e8196c08f7b635e68f2c6a5658d195391
nimcasib <32439405+nimcasib@users.noreply.github.com>
2023-01-25 09:03:34 +0300

    Add files via upload

---
+++ DZ/DZ/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.NumberInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(378, 167);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(35, 13);
+            this.Result.TabIndex = 1;
+            this.Result.Text = "label1";
+            // 
+            // NumberInput
+            // 
+            this.NumberInput.Location = new System.Drawing.Point(342, 183);
+            this.NumberInput.Name = "NumberInput";
+            this.NumberInput.Size = new System.Drawing.Size(100, 20);
+            this.NumberInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NumberInput);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox NumberInput;
+    }
+}
+
---
+++ DZ/DZ/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ
+{
+    public partial class Form1 : Form
+    {
+        double calculation=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberInput = 0; //переменная, считывающая введённой число
+            double root = 0; //переменная для извлечения квадратного корня из введённого числа
+
+            if(NumberInput.Text!="")
+                numberInput = Convert.ToInt32(NumberInput.Text); //преобразование типов данных
+
+            root = Math.Sqrt(numberInput); //расчтет квадратного корня из введённого числа 
+            calculation += root; //сложение суммы квадратных корней введённых чисел
+            calculation= Math.Round(calculation, 0); // округление
+            Result.Text = calculation.ToString(); //преобразование инта в стринг
+            NumberInput.Text = null; //сбрасывание строки ввода при нажатии на кнопку
+        }
+    }
+}
